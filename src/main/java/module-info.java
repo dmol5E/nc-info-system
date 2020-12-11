592e70d897675a74318a1c03d19ad6090ae87f38
dark231321 <89053010376Dan>
2020-12-11 20:15:58 +0400

    updated generalize Repository

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module infosystem {
-    requires javafx.controls;
-    requires javafx.fxml;
     requires org.apache.logging.log4j;
     requires com.google.gson;
+    requires javafx.controls;
+    requires javafx.fxml;
 
     opens com.nc.unc to javafx.fxml;
     exports com.nc.unc;
